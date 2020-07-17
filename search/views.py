44f4bc9756ca0d299d7312b8accd3b7ec648e03c
Zehan <zehan.kareem@gmail.com>
2020-07-18 00:23:43 +0600

    Search optimized 1 more step

--- search/views.py
+++ search/views.py
@@ -4,13 +4,17 @@ from shop.models import Items
 
 def searchitems(request):
     if request.method == 'GET':
-        query = request.GET.get('q')
+        query = request.GET.get('q').split(' ')
         submitbutton= request.GET.get('submit')
-
+        n = len(query)
         if query is not None:
-            matchup = Q(title__icontains=query)  \
-                      | Q(sub_category__sub_category_name__icontains=query) \
-                      | Q(sub_category__category__category_name__icontains=query)
+            matchup = Q(title__icontains=query[0]) | Q(title__icontains=query[n-1])  \
+                      | Q(sub_category__sub_category_name__icontains=query[0]) \
+                      | Q(sub_category__category__category_name__icontains=query[0])\
+                      | Q(title__icontains=query[0]) & Q(sub_category__sub_category_name__icontains=query[n-1])\
+                      | Q(sub_category__sub_category_name__icontains=query[0]) & Q(title__icontains=query[n-1])\
+                      | Q(title__icontains=query[0]) & Q(sub_category__category__category_name__icontains=query[n-1])\
+
 
             results = Items.objects.filter(matchup).distinct()
             context={'results': results,
